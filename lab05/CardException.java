22b93c125be54e31f47144ee90c8aa4c3194ef6b
tug98850 <39741492+tug98850@users.noreply.github.com>
2018-10-20 23:28:09 -0400

    Create CardException.java
    Throw a CardException in Card class whenever user fails to input the value and suit in an appropriate manner. Refer to the Card class to see how this CardException is used.

---
+++ lab05/CardException.java
@@ -0,0 +1,23 @@
+/*
+Lab 05
+
+Author: Paul LaFollette
+
+Course number: 1968
+
+Revision:
+
+   20181009(PL): original version
+
+*/
+
+
+
+
+public class CardException extends Exception
+{
+  public CardException(String s)
+  {
+    super(s);
+  }
+}
